f37ddb01dc3cd50c7a3cd1786024335d0c98d91b
Assaad <asadmfarooq99@gmail.com>
2026-06-19 17:40:33 +0500

    Update opm.ps1

--- oh-my-posh/opm.ps1
+++ oh-my-posh/opm.ps1
@@ -1,5 +1,5 @@
 oh-my-posh init pwsh --config "C:\Users\gingl\catppuccin_mocha.omp.json" | Invoke-Expression
-------------------------------------------------------------------------------------------------
+
 <# paste it in your $PROFILE
 Type: nvim $PROFILE
 #>
